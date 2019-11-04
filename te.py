dbaba2676896c2af6931ed783d177bbdda91c663
sozsfness <awakei@outlook.com>
2019-11-03 20:10:37 -0800

    rm

--- te.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import numpy as np
-img = Image.open('044_00142.png')
-arr = np.asarray(img.convert('RGB'))
-tosave = Image.fromarray(arr.astype('uint8'))
-tosave.save('saved.png')
